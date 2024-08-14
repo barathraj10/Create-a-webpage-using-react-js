5d7addf0bdfe3f1b286590b3db38edc249537a7c
barathraj10 <barathraj.offl@gmail.com>
2024-08-14 14:49:27 +0530

    Updates

---
+++ precache-manifest.a27a625498232a941f518ac542bf11a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b86b11205ab1b58f173c0c5bcbf3a92",
+    "url": "/barathraj10/Create-a-webpage-using-react-js/index.html"
+  },
+  {
+    "revision": "9114111433e91655c0be",
+    "url": "/barathraj10/Create-a-webpage-using-react-js/static/css/main.e16cb0ce.chunk.css"
+  },
+  {
+    "revision": "78447bc438c4daf2096b",
+    "url": "/barathraj10/Create-a-webpage-using-react-js/static/js/2.7bf6eb88.chunk.js"
+  },
+  {
+    "revision": "9114111433e91655c0be",
+    "url": "/barathraj10/Create-a-webpage-using-react-js/static/js/main.cb317aa1.chunk.js"
+  },
+  {
+    "revision": "db444dbb67110f42f217",
+    "url": "/barathraj10/Create-a-webpage-using-react-js/static/js/runtime~main.ad21c80f.js"
+  }
+]);
